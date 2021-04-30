b61804571c5848346949097cdec1570c41636040
Michael Janicki <mjanicki@sittercity.com>
2021-04-30 14:30:49 -0500

    let method

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,5 +1,5 @@
 class Card
-    attr_reader :rank, :suit
+    attr_accessor :rank, :suit
     def initialize(rank, suit)
         @rank = rank
         @suit = suit
@@ -8,16 +8,16 @@ class Card
 end
 
 RSpec.describe Card do
-    before do
-        @card = Card.new('Ace', 'Spades')
-    end
+    let(:card) { Card.new('Ace', 'Spades') }
 
     it 'has a rank' do
-        expect(@card.rank).to eq('Ace')
+        expect(card.rank).to eq('Ace')
+        card.rank = 'Queen'
+        expect(card.rank).to eq('Queen')
     end 
 
     it 'has a suit' do
-        expect(@card.suit).to eq('Spades')
+        expect(card.suit).to eq('Spades')
     end
 end
 
