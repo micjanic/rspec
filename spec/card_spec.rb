60f50824e5d5ef3cd966de6f283d9a17a044c6be
Michael Janicki <mjanicki@sittercity.com>
2021-04-30 11:09:25 -0500

    pass test

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,8 +1,15 @@
+class Card
+    attr_reader :type
+    def initialize(type)
+        @type = type
+    end
+
+end
+
 RSpec.describe 'Card' do 
    it 'has a type' do
         card = Card.new('Ace of Spades')
-        expect(card.type).to eq('Ace of Spades')
-
-        expect(1 + 1).to(eq(2))
+        expect(card.type).to eq('Ace of Spades') 
    end 
 end
+
