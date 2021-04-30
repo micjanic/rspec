c179fc6f957429dd9d849c4ac8a81bac1367ae71
Michael Janicki <mjanicki@sittercity.com>
2021-04-30 11:25:35 -0500

    multiple expects

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,15 +1,22 @@
 class Card
-    attr_reader :type
-    def initialize(type)
-        @type = type
+    attr_reader :rank, :suit
+    def initialize(rank, suit)
+        @rank = rank
+        @suit = suit
     end
 
 end
 
-RSpec.describe 'Card' do 
-   it 'has a type' do
-        card = Card.new('Ace of Spades')
-        expect(card.type).to eq('Ace of Spades') 
-   end 
+RSpec.describe Card do 
+    
+   it 'has a rank' do
+        card = Card.new('Ace', 'Spades')
+        expect(card.rank).to eq('Ace')
+    end 
+    
+    it 'has a suit' do
+        card = Card.new('Ace', 'Spades')
+        expect(card.suit).to eq('Spades')
+   end
 end
 
