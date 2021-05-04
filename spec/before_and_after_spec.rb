602574741ccafb646af77123ed505f47a1bbdf4f
Michael Janicki <mjanicki@sittercity.com>
2021-05-04 16:40:24 -0500

    nested hooks

--- spec/before_and_after_spec.rb
+++ spec/before_and_after_spec.rb
@@ -1,24 +1,24 @@
 RSpec.describe 'before and after hooks' do
-    before(:context) do
-        puts 'before context'
-    end
+  before(:context) do
+    puts 'before context'
+  end
 
-    after(:context) do
-        puts 'after context'
-    end
+  after(:context) do
+    puts 'after context'
+  end
 
-    before(:example) do
-        puts 'before example'
-    end
+  before(:example) do
+    puts 'before example'
+  end
 
-    after(:example) do
-        puts 'after example'
-    end
+  after(:example) do
+    puts 'after example'
+  end
 
-    it 'is just a random example' do
-        expect(5 * 4).to eq(20)
-    end
-    it 'is just another random example' do
-        expect(3 - 2).to eq(1)
-    end
+  it 'is just a random example' do
+    expect(5 * 4).to eq(20)
+  end
+  it 'is just another random example' do
+    expect(3 - 2).to eq(1)
+  end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,23 +1,22 @@
 class Card
-    attr_accessor :rank, :suit
-    def initialize(rank, suit)
-        @rank = rank
-        @suit = suit
-    end
+  attr_accessor :rank, :suit
 
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
 end
 
 RSpec.describe Card do
-    let(:card) { Card.new('Ace', 'Spades') }
+  let(:card) { Card.new('Ace', 'Spades') }
 
-    it 'has a rank' do
-        expect(card.rank).to eq('Ace')
-        card.rank = 'Queen'
-        expect(card.rank).to eq('Queen')
-    end 
+  it 'has a rank' do
+    expect(card.rank).to eq('Ace')
+    card.rank = 'Queen'
+    expect(card.rank).to eq('Queen')
+  end
 
-    it 'has a suit' do
-        expect(card.suit).to eq('Spades')
-    end
+  it 'has a suit' do
+    expect(card.suit).to eq('Spades')
+  end
 end
-
