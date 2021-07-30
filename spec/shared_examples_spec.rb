2401cbcc501eae683c7178f7bd761e02564d50c8
Michael Janicki <mjanicki@sittercity.com>
2021-07-30 15:59:51 -0500

    shared example functionality

--- spec/shared_examples_spec.rb
+++ spec/shared_examples_spec.rb
@@ -1,5 +1,12 @@
+RSpec.shared_examples 'a Ruby object with length property that returns 3' do
+  it 'returns the number of items' do
+    expect(subject.length).to eq(3) # subject is lazily loaded, so doesn't matter that it isn't defined yet, when injected into a subject block it will get the current subject
+  end
+end
+
 RSpec.describe Array do
   subject { [1, 2, 3] }
+  include_examples 'a Ruby object with length property that returns 3'
 end
 
 RSpec.describe String do
@@ -8,6 +15,7 @@ end
 
 RSpec.describe Hash do
   subject { { a: 1, b: 2, c: 3 } }
+  include_examples 'a Ruby object with length property that returns 3'
 end
 
 class SausageLink
@@ -17,5 +25,6 @@ class SausageLink
 end
 
 RSpec.describe SausageLink do
-    subject {described_class.new}
+  subject { described_class.new }
+  include_examples 'a Ruby object with length property that returns 3'
 end
